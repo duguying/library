b1f19951cb378f9fa422f95bc48dab34c6fe1d14
duguying <duguying2008@gmail.com>
2013-12-09 17:54:47 +0800

    change SqlHelper.cs

--- library/Library.DAL/ReaderTypeDAL.cs
+++ library/Library.DAL/ReaderTypeDAL.cs
@@ -14,7 +14,26 @@ namespace Library.DAL
         #region 表基本操作
         public static int Add(ReaderType reader_type_record)
         {
-            return 0;
+            int rows = 0;
+
+            #region SQL 语句准备
+            string sql = "insert into TB_ReaderType(rdType,rdTypeName,maxBorrowNum,maxBorrowDay,maxContinueTimes)values(@rdType,@rdTpyeName,@maxBorrowNum,@maxBorrowDay,@maxContinueTimes)";
+            SqlParameter[] parameters = { 
+                                            new SqlParameter("@rdType",reader_type_record.rdType),
+                                            new SqlParameter("@rdTypeName",reader_type_record.rdTypeName),
+                                            new SqlParameter("@maxBorrowNum",reader_type_record.maxBorrowNum),
+                                            new SqlParameter("@maxBorrowDay",reader_type_record.maxBorrowDay),
+                                            new SqlParameter("@maxContinueTimes",reader_type_record.maxContinueTimes),
+                                        };
+            #endregion
+
+            try {
+                rows = SqlHelper.ExecuteNonQuery(sql, parameters);
+            }catch(SqlException e){
+                throw new Exception(e.Message);
+            }
+
+            return rows;
         }
         public static int Delete(ReaderType reader_type_record)
         {
